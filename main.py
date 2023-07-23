dbaf83f94aae62ae121fdc942c16dde17d0a80fc
DuQer <marcinurbanski100@gmail.com>
2023-07-23 23:13:04 +0200

    Update user function

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ app.config['JWT_SECRET_KEY'] = 'secret-key'
 jwt = JWTManager(app)
 
 refresh_tokens = {}
+
+
 @app.route('/create-user', methods=['POST'])
 def create_user():
     username = request.json.get('username')
@@ -39,6 +41,37 @@ def create_user():
     access_token = create_access_token(identity=username)
 
     return {'access_token': access_token}, 201
+
+
+@app.route('/update-user/<int:user_id>', methods=['PUT'])
+@jwt_required()
+def update_user(user_id):
+    current_user_id = get_jwt_identity()
+    new_username = request.json.get('new_username')
+    print(current_user_id)
+    print(user_id)
+    if not new_username:
+        return {'message': 'New username is required'}, 400
+
+    conn = sqlite3.connect('database/database.db')
+    c = conn.cursor()
+
+    c.execute('SELECT * FROM users WHERE id=?', (user_id,))
+    user = c.fetchone()
+
+    if not user:
+        return {'message': 'User not found'}, 404
+
+    c.execute('SELECT * FROM users WHERE username=?', (new_username,))
+    existing_user = c.fetchone()
+    if existing_user:
+        return {'message': 'Username already exists'}, 409
+
+    c.execute('UPDATE users SET username=? WHERE id=?', (new_username, user_id))
+    conn.commit()
+    conn.close()
+
+    return {'message': 'Username updated successfully'}, 200
 
 
 @app.route('/refresh-token', methods=['POST'])
