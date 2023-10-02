1f9e4bae89f93106d54d90fce846e336d93e9d1f
DuQer <marcinurbanski100@gmail.com>
2023-10-02 22:47:57 +0200

    Implemented 'UpdateUserForm' & 'CreateTaskForm' for validating data in the 'update_user' and  'create_task' functions respectively.

--- forms.py
+++ forms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, PasswordField, TextAreaField, DateField, IntegerField
+from wtforms.validators import DataRequired, Length, Optional
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+
+
+class UpdateUserForm(FlaskForm):
+    new_username = StringField('New Username', validators=[DataRequired(), Length(min=4, max=20)])
+
+
+class CreateTaskForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(max=255)])
+    description = TextAreaField('Description', validators=[Optional(), Length(max=1000)])
+    due_date = DateField('Due Date', validators=[Optional()])
+    priority = IntegerField('Priority', validators=[Optional()])
