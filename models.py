00f10db218c60cb4da17d1ff31a936e92fce5e09
DuQer <marcinurbanski100@gmail.com>
2023-10-04 23:15:00 +0200

    Created ORM model for tasks

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from main import db
 
 
@@ -6,3 +7,13 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     tasks = db.relationship('Task', backref='user', lazy=True)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    creation_time = db.Column(db.DateTime, default=datetime.utcnow)
+    due_date = db.Column(db.DateTime)
+    priority = db.Column(db.Integer)
