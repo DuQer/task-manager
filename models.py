24915029f7523efd8e127b96c14cd7557a25af35
DuQer <marcinurbanski100@gmail.com>
2023-10-05 23:43:24 +0200

    Resolved circular imports in models.py

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from main import db
 
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
@@ -10,10 +11,12 @@ class User(db.Model):
 
 
 class Task(db.Model):
+    __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text)
     creation_time = db.Column(db.DateTime, default=datetime.utcnow)
     due_date = db.Column(db.DateTime)
     priority = db.Column(db.Integer)
+
